742b51bae5dd6be453de363ba4b92c693050350f
satorux <satorux@chromium.org>
2010-02-15 15:01:55 +0900

    Do not include chromeos/libcros.so in chrome-chromeos.zip.
    The original pattern "chrome-chromeos/chrome*" was loose enough
    to include "chromeos/libcros.so" if it's present. The file should
    not be included in chrome-chromeos.zip file, which is used to build
    the chromeos-chrome deb package, since libcros.so is provided by
    the chromeos-libcros deb package.
    
    I had "chromium/src/out/Release/chromeos/libcros.so" to run the
    Chromium OS version of Chromium browser locally with libcros.so
    enabled.
    
    TEST=manually:
    % cd ../build/x86/local_assets
    % unzip -l chrome-chromeos.zip > before
    ... made this change ...
    % cd ../build/x86/local_assets
    unzip -l chrome-chromeos.zip > after
    % diff -u before after
    --- before       2010-01-22 13:53:39.361522119 +0900
    +++ after        2010-01-22 13:54:52.117212719 +0900
    @@ -2,8 +2,6 @@
       Length      Date    Time    Name
     ---------  ---------- -----   ----
      12584626  2010-01-22 13:46   chrome-chromeos/candidate_window
    -        0  2010-01-22 13:13   chrome-chromeos/chromeos/
    -   432316  2010-01-22 13:13   chrome-chromeos/chromeos/libcros.so
          4850  2009-12-09 15:37   chrome-chromeos/product_logo_48.png
         54594  2010-01-22 13:46   chrome-chromeos/session
             0  2010-01-18 11:52   chrome-chromeos/resources/
    @@ -220,4 +218,4 @@
        127262  2010-01-21 14:05   chrome-chromeos/locales/ro.pak
        112422  2010-01-21 14:05   chrome-chromeos/locales/en-GB.pak
     ---------                     -------
    - 66476904                     218 files
    + 66044588                     216 files
    
    Review URL: http://codereview.chromium.org/552101

--- build_chrome.sh
+++ build_chrome.sh
@@ -88,7 +88,10 @@ ln -s $BUILD_DIR/$FLAGS_mode $CHROME_LINUX_DIR
 echo Zipping $CHROME_LINUX_DIR to $OUTPUT_ZIP
 cd $BUILD_DIR
 rm -f $OUTPUT_ZIP
-zip -r1 $OUTPUT_ZIP chrome-chromeos -i "chrome-chromeos/chrome*" \
+zip -r1 $OUTPUT_ZIP chrome-chromeos -i \
+  "chrome-chromeos/chrome" \
+  "chrome-chromeos/chrome.*" \
+  "chrome-chromeos/chrome-*" \
   "chrome-chromeos/candidate_window" \
   "chrome-chromeos/libffmpegsumo.so" "chrome-chromeos/xdg-settings" \
   "chrome-chromeos/locales/*" "chrome-chromeos/resources/*" \
