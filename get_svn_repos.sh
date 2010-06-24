b59dd19d2d8af4e71cac994578d4704a01f7060b
David McMahon <djmm@chromium.org>
2010-06-24 16:21:26 -0700

    Add missing svn repos
    Review URL: http://codereview.chromium.org/2824030

--- get_svn_repos.sh
+++ get_svn_repos.sh
@@ -11,3 +11,9 @@ svn checkout http://src.chromium.org/svn/trunk/src/build@36775 src/third_party/c
 svn checkout http://o3d.googlecode.com/svn/trunk/googleclient/third_party/vectormath@166 src/third_party/vectormath
 svn checkout http://v8.googlecode.com/svn/trunk@4565 src/third_party/v8
 svn checkout http://gyp.googlecode.com/svn/trunk@824 src/third_party/gyp/files
+ # O3D selenium tests
+svn checkout http://o3d.googlecode.com/svn/trunk/googleclient/third_party/selenium_rc@178 src/third_party/autotest/files/client/site_tests/graphics_O3DSelenium/O3D/third_party/selenium_rc
+svn checkout http://src.chromium.org/svn/trunk/src/o3d/tests/selenium@44717 src/third_party/autotest/files/client/site_tests/graphics_O3DSelenium/O3D/o3d/tests/selenium
+svn checkout http://src.chromium.org/svn/trunk/src/o3d/samples@46579 src/third_party/autotest/files/client/site_tests/graphics_O3DSelenium/O3D/o3d/samples
+svn checkout http://o3d.googlecode.com/svn/trunk/googleclient/o3d_assets/tests@155 src/third_party/autotest/files/client/site_tests/graphics_O3DSelenium/O3D/o3d/o3d_assets/tests
+svn checkout http://google-gflags.googlecode.com/svn/trunk@29 src/third_party/autotest/files/client/site_tests/graphics_O3DSelenium/O3D/o3d/third_party/gflags
