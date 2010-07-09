f9acd8e3d378d193704475e81e5910413d4d9b02
Mandeep Singh Baines <msb@chromium.org>
2010-07-09 13:27:14 -0700

    get_svn_repos.sh: remove repos that are no longer needed
    This change removes svn repos which have had their ebuild modified
    to use the subversion eclass. This repos have already been removed
    from the DEPS file.
    
    Change-Id: I290d0bc6e401511abb7d637d83fe2d6961851aa9
    
    Review URL: http://codereview.chromium.org/2899013

--- get_svn_repos.sh
+++ get_svn_repos.sh
@@ -2,15 +2,8 @@
 
 #Temp Hack to get SVN repos for ebuilds that haven't switched to tar balls or getting code via SVN.
 
-svn checkout http://mozc.googlecode.com/svn/trunk@24 src/third_party/ibus-mozc/files
-svn checkout http://google-breakpad.googlecode.com/svn/trunk@598 src/third_party/google-breakpad/files
 svn checkout http://shflags.googlecode.com/svn/tags/1.0.3@137 src/third_party/shflags/files
 svn checkout http://shunit2.googlecode.com/svn/tags/source/2.1.5@294 src/third_party/shunit2/files
-svn checkout http://src.chromium.org/svn/trunk/src/base@36775 src/third_party/chrome/files/base
-svn checkout http://src.chromium.org/svn/trunk/src/build@36775 src/third_party/chrome/files/build
-svn checkout http://o3d.googlecode.com/svn/trunk/googleclient/third_party/vectormath@166 src/third_party/vectormath
-svn checkout http://v8.googlecode.com/svn/trunk@4565 src/third_party/v8
-svn checkout http://gyp.googlecode.com/svn/trunk@824 src/third_party/gyp/files
  # O3D selenium tests
 svn checkout http://o3d.googlecode.com/svn/trunk/googleclient/third_party/selenium_rc@178 src/third_party/autotest/files/client/site_tests/graphics_O3DSelenium/O3D/third_party/selenium_rc
 svn checkout http://src.chromium.org/svn/trunk/src/o3d/tests/selenium@44717 src/third_party/autotest/files/client/site_tests/graphics_O3DSelenium/O3D/o3d/tests/selenium
