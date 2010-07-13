8ae113bb423956a820b40a76e5f1191aa907b399
Tan Gao <tgao@chromium.org>
2010-07-13 07:04:48 -0700

    remove handling of kernel verification block from recovery image
    Change-Id: I749dc652651fdb02b49b290863e2fa9adc6b904e
    
    Review URL: http://codereview.chromium.org/2948005

--- mod_image_for_recovery.sh
+++ mod_image_for_recovery.sh
@@ -46,11 +46,6 @@ mount_gpt_cleanup() {
 # Modifies an existing image for recovery use
 update_recovery_packages() {
   local image_name=$1
-  local sector_size=512  # sector size in bytes
-  local num_sectors_vb=128  # number of sectors in kernel verification blob
-  # Start offset of kernel A (aligned to 4096-sector boundary)
-  local start_kern_a=4096
-  local vb_file="${STATEFUL_FS_DIR}/verification_blob.kernel"
 
   echo "Modifying image ${image_name} for recovery use"
 
@@ -62,19 +57,6 @@ update_recovery_packages() {
 
   # Mark the image as a recovery image (needed for recovery boot)
   sudo touch "${STATEFUL_FS_DIR}/.recovery"
-
-  # Copy verification blob out of kernel A into stateful partition
-  # so that we can restore it during recovery
-  sudo touch $vb_file
-  echo "Backing up kernel verification blob onto stateful partition ..."
-  sudo dd if="$image_name" of="$vb_file" skip=$start_kern_a bs=$sector_size \
-      count=$num_sectors_vb conv=notrunc
-
-  # Overwrite verification blob with recovery image verification blob
-  # TODO(tgao): resign kernel for recovery image
-  echo "Overwrite kernel verification blob with resigned blob for recovery..."
-  sudo dd if=/dev/zero of="$image_name" seek=$start_kern_a bs=$sector_size \
-      count=$num_sectors_vb conv=notrunc
 
   trap - EXIT
   ${SCRIPTS_DIR}/mount_gpt_image.sh -u -r "${ROOT_FS_DIR}" \
