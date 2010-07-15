6d9d7a61e315593c04a2fc57d8c389756b6c5207
David McMahon <djmm@chromium.org>
2010-07-15 16:37:06 -0700

    Update Minor version to 8 - this should have happened after R7
    Review URL: http://codereview.chromium.org/3038003

--- chromeos_version.sh
+++ chromeos_version.sh
@@ -19,7 +19,7 @@ HOSTNAME=$(hostname)
 # Major/minor versions.
 # Primarily for product marketing.
 export CHROMEOS_VERSION_MAJOR=0
-export CHROMEOS_VERSION_MINOR=7
+export CHROMEOS_VERSION_MINOR=8
 
 # Branch number.
 # Increment by 1 in a new release branch.
