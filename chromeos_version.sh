46bbd36ec8e2da082b8b990130aff88017b28a55
Scott Zawalski <scottz@google.com>
2010-08-25 23:04:25 -0700

    Created branch 0.8.68.B.  Update CHROMEOS_VERSION_BRANCH=69
    Change-Id: I775d399a3ebe24caf5227406e7baf5a7025f0932

--- chromeos_version.sh
+++ chromeos_version.sh
@@ -26,7 +26,7 @@ export CHROMEOS_VERSION_MINOR=8
 # Increment by 2 in trunk after making a release branch.
 # Does not reset on a major/minor change (always increases).
 # (Trunk is always odd; branches are always even).
-export CHROMEOS_VERSION_BRANCH=67
+export CHROMEOS_VERSION_BRANCH=69
 
 # Patch number.
 # Increment by 1 each release on a branch.
