ef45f16aa23dd955f2b55891b2c648f53262352d
Ken Mixter <kmixter@chromium.org>
2010-09-14 17:19:33 -0700

    Allow prepackaged to still be passed and make learn_board work again
    Change-Id: I664858ff9b931479fe2fac89a24aa3e7fce8493b
    
    BUG=
    TEST=ran rrt without ==board or default board set
    
    Review URL: http://codereview.chromium.org/3431004

--- run_remote_tests.sh
+++ run_remote_tests.sh
@@ -154,16 +154,14 @@ function main() {
 
   remote_access_init
 
-  # HACK: Temporary hack for cros-workon conversion
-  # In cros-workon, we always use the prepackaged tests, because the locations
-  # where tests come from are possibly infinite.
-  [[ -n "${CROS_WORKON_SRCROOT}" ]] && \
-    FLAGS_prepackaged_autotest="/build/${FLAGS_board}/usr/local/autotest/"
-
   local autotest_dir=""
   if [[ -z "${FLAGS_prepackaged_autotest}" ]]; then
     learn_board
-    autotest_dir="${GCLIENT_ROOT}/src/third_party/autotest/files"
+    if [[ -n "${CROS_WORKON_SRCROOT}" ]]; then
+      autotest_dir="/build/${FLAGS_board}/usr/local/autotest"
+    else
+      autotest_dir="${GCLIENT_ROOT}/src/third_party/autotest/files"
+    fi
   else
     autotest_dir="${FLAGS_prepackaged_autotest}"
   fi
