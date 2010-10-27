604a3f31681b68d5b342f196c5c5facac0398a1a
Chris Sosa <sosa@chromium.org>
2010-10-27 11:51:02 -0700

    Adding tracking branch broke repo sync on full builders.
    Change-Id: Iff1a416a1f7c37870fbcd42ed4aaf6f96f1ea11d
    
    BUG=8245
    TEST=Ran with --clobber
    
    Review URL: http://codereview.chromium.org/4128007

--- bin/cros_repo_sync_all.py
+++ bin/cros_repo_sync_all.py
@@ -12,7 +12,7 @@ import optparse
 import sys
 
 """Number of retries to retry repo sync before giving up"""
-_NUMBER_OF_RETRIES=3
+_NUMBER_OF_RETRIES = 3
 
 def main():
   parser = optparse.OptionParser()
@@ -20,16 +20,18 @@ def main():
                     help='root directory where sync occurs')
   parser.add_option('-c', '--clobber', action='store_true', default=False,
                     help='clobber build directory and do a full checkout')
+  parser.add_option('-t', '--tracking_branch', default='cros/master',
+                    help='Branch to sync against for full checkouts.')
   (options, args) = parser.parse_args()
   if options.buildroot:
     if options.clobber:
-      cbuildbot._FullCheckout(options.buildroot, rw_checkout=False,
-          retries=_NUMBER_OF_RETRIES)
+      cbuildbot._FullCheckout(options.buildroot, options.tracking_branch,
+                              rw_checkout=False, retries=_NUMBER_OF_RETRIES)
     else:
       cbuildbot._IncrementalCheckout(options.buildroot, rw_checkout=False,
           retries=_NUMBER_OF_RETRIES)
   else:
-    print >>sys.stderr, 'ERROR:  Must set buildroot'
+    print >> sys.stderr, 'ERROR:  Must set buildroot'
     sys.exit(1)
 
 if __name__ == '__main__':
