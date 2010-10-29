142e452d37b6aef7cba574355b30d128ea68c0a7
Chris Sosa <sosa@chromium.org>
2010-10-29 11:58:15 -0700

    Fixing of the symlinks isn't being called because rootfs is being remounted ro.
    This is being remounted ro in cros_make_image_bootable so I should be checking
    for the stateful mount pt not the rootfs mountpt
    
    Change-Id: I1ee64489516fae10a6246c5d79236c8b5df090ee
    
    BUG=8116
    TEST=Ran it and ran bin/cros_start vm and inspected /usr/local
    
    Review URL: http://codereview.chromium.org/4148013

--- mount_gpt_image.sh
+++ mount_gpt_image.sh
@@ -47,7 +47,7 @@ function unmount_image() {
   # Don't die on error to force cleanup
   set +e
   # Reset symlinks in /usr/local.
-  if mount | egrep ".* ${FLAGS_rootfs_mountpt} .*\(rw,"; then
+  if mount | egrep ".* ${FLAGS_stateful_mountpt} .*\(rw,"; then
     setup_symlinks_on_root "/usr/local" "/var" \
       "${FLAGS_stateful_mountpt}"
     fix_broken_symlinks "${FLAGS_rootfs_mountpt}"
