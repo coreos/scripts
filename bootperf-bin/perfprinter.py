b4c138244f9cf2f95ee0d0c0150290e3099b0cb0
J. Richard Barnette <jrbarnette@chromium.org>
2011-06-14 15:58:28 -0700

    Refactor the results directory reading into a separate module.
    This change is made in anticipation of needing more than one kind
    of reader in future. Most notably, a reader to extract data from
    the MySQL results database would be a nice addition.
    
    BUG=None
    TEST=run showbootdata on any pre-existing data set
    
    Change-Id: I5811aa1427a2714ce6ba392cdec584b4236162fd
    Reviewed-on: http://gerrit.chromium.org/gerrit/2644
    Reviewed-by: Mike Truty <truty@chromium.org>
    Tested-by: Richard Barnette <jrbarnette@chromium.org>

--- bootperf-bin/perfprinter.py
+++ bootperf-bin/perfprinter.py
@@ -9,72 +9,18 @@ import os
 import os.path
 import re
 
-import resultset
 
-
-_PERF_KEYVAL_PATTERN = re.compile("(.*){perf}=(.*)\n")
-
-
-def ReadKeyvalFile(results, file_):
-  """Read an autotest keyval file, and process the results.
-
-  The `file_` parameter is a file object with contents in autotest
-  perf keyval format:
-      <keyname>{perf}=<value>
-
-  Each iteration of the test is terminated with a single blank line,
-  including the last iteration.  Each iteration's results are added
-  to the `results` parameter, which should be an instance of
-  TestResultSet.
-
-  """
-  kvd = {}
-  for line in iter(file_):
-    if line == "\n":
-      results.AddIterationResults(kvd)
-      kvd = {}
-      continue
-    m = _PERF_KEYVAL_PATTERN.match(line)
-    if m is None:
-      continue
-    kvd[m.group(1)] = m.group(2)
-
-
-_RESULTS_PATH = (
-    "summary/platform_BootPerfServer/platform_BootPerfServer/results/keyval")
-
-
-def ReadResultsDirectory(dir_):
-  """Process results from a 'bootperf' output directory.
-
-  The accumulated results are returned in a newly created
-  TestResultSet object.
-
-  """
-  res_set = resultset.TestResultSet(dir_)
-  dirlist = fnmatch.filter(os.listdir(dir_), "run.???")
-  dirlist.sort()
-  for run in dirlist:
-    keyval_path = os.path.join(dir_, run, _RESULTS_PATH)
-    try:
-      kvf = open(keyval_path)
-    except IOError:
-      continue
-    ReadKeyvalFile(res_set, kvf)
-  res_set.FinalizeResults()
-  return res_set
-
-
-def PrintRawData(dirlist, use_timestats, keylist):
+def PrintRawData(reader, dirlist, use_timestats, keylist):
   """Print 'bootperf' results in "raw data" format."""
   for dir_ in dirlist:
+    results = reader(dir_)
     if use_timestats:
-      keyset = ReadResultsDirectory(dir_).TimeKeySet()
+      keyset = results.TimeKeySet()
     else:
-      keyset = ReadResultsDirectory(dir_).DiskKeySet()
+      keyset = results.DiskKeySet()
     for i in range(0, keyset.num_iterations):
       if len(dirlist) > 1:
-        line = "%s %3d" % (dir_, i)
+        line = "%s %3d" % (results.name, i)
       else:
         line = "%3d" % i
       if keylist is not None:
@@ -87,7 +33,7 @@ def PrintRawData(dirlist, use_timestats, keylist):
       print line
 
 
-def PrintStatisticsSummary(dirlist, use_timestats, keylist):
+def PrintStatisticsSummary(reader, dirlist, use_timestats, keylist):
   """Print 'bootperf' results in "summary of averages" format."""
   if use_timestats:
     header = "%5s %3s  %5s %3s  %s" % (
@@ -99,10 +45,11 @@ def PrintStatisticsSummary(dirlist, use_timestats, keylist):
     format = "%6s %2d%%  %6s %2d%%  %s"
   havedata = False
   for dir_ in dirlist:
+    results = reader(dir_)
     if use_timestats:
-      keyset = ReadResultsDirectory(dir_).TimeKeySet()
+      keyset = results.TimeKeySet()
     else:
-      keyset = ReadResultsDirectory(dir_).DiskKeySet()
+      keyset = results.DiskKeySet()
     if keylist is not None:
       markers = keylist
     else:
@@ -110,7 +57,7 @@ def PrintStatisticsSummary(dirlist, use_timestats, keylist):
     if havedata:
       print
     if len(dirlist) > 1:
-      print "%s" % dir_,
+      print "%s" % results.name,
     print "(on %d cycles):" % keyset.num_iterations
     print header
     prevvalue = 0
