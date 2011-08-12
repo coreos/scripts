aaef76166f151a12249280c6b505b9af512cb190
J. Richard Barnette <jrbarnette@chromium.org>
2011-08-11 17:10:28 -0700

    Convert build_library/test_image to a shell library.
    Renamed the fuction from "test_image" to "test_image_content";
    renamed the source file to match.
    
    BUG=None
    TEST=build both x86 and arm images
    
    Change-Id: I158f2c5bc0f2fc260d48bd125a1899e6a21d7b79
    Reviewed-on: http://gerrit.chromium.org/gerrit/5821
    Reviewed-by: Vince Laviano <vlaviano@chromium.org>
    Tested-by: Richard Barnette <jrbarnette@chromium.org>

---
+++ build_library/test_image_content.sh
@@ -0,0 +1,36 @@
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+test_image_content() {
+  local root="$1"
+  local returncode=0
+
+  local binaries=(
+    "$root/usr/bin/Xorg"
+    "$root/usr/bin/chromeos-wm"
+    "$root/boot/vmlinuz"
+    "$root/sbin/session_manager"
+    "$root/bin/sed"
+    "$root/opt/google/chrome/chrome"
+  )
+
+  for test_file in "${binaries[@]}"; do
+    if [ ! -f "$test_file" ]; then
+      error "test_image_content: Cannot find '$test_file'"
+      returncode=1
+    fi
+  done
+
+  local libs=( $(sudo find "$root" -type f -name '*.so*') )
+
+  # Check that all .so files, plus the binaries, have the appropriate
+  # dependencies.
+  local check_deps="${BUILD_LIBRARY_DIR}/check_deps"
+  if ! "$check_deps"  "$root" "${binaries[@]}" "${libs[@]}"; then
+    error "test_image_content: Failed dependency check"
+    returncode=1
+  fi
+
+  return $returncode
+}
