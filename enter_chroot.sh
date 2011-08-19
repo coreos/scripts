cc6d2e7f8591df6a5540ddb83144f61544b6e1e1
Zdenek Behan <zbehan@chromium.org>
2011-08-20 01:18:03 +0200

    crosutils: switch enter_chroot.sh and make_chroot for placeholders
    * This CL deprecates the use of enter_chroot and make_chroot completely,
    leaving the functionality exposed only through cros_sdk.
    
    BUG=chromium-os:18750
    TEST=run them
    
    Change-Id: I864960b4e25245341431c3a3950638fa569820ed
    Reviewed-on: http://gerrit.chromium.org/gerrit/6358
    Reviewed-by: Anush Elangovan <anush@chromium.org>
    Tested-by: Zdenek Behan <zbehan@chromium.org>

--- enter_chroot.sh
+++ enter_chroot.sh
@@ -1,1 +1,8 @@
-sdk_lib/enter_chroot.sh
+#!/bin/bash
+
+# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+echo "This command is deprecated, please run cros_sdk $*"
+exit 1
