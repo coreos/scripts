597a79dc858315d4d6fcaca07abc9ba5b9882493
Darin Petkov <petkov@chromium.org>
2011-11-24 11:30:12 +0100

    Fail 'build_image test' if a mod-for-test script fails.
    It seems better to catch issues earlier during the build phase rather than later
    during the actual test run. Prompted to do this after debugging
    crosbug.com/21837.
    
    BUG=chromium-os:23422
    TEST=build_image test for x86-alex, amd64-generic, tegra2_kaen with no failures;
    build_image factory_test for x86-alex with no failures; also verified that
    build_image exits early with a non-zero exit code for amd64-generic without the
    fix for crosbug.com/21837.
    
    Change-Id: Ibdcf770d1d854d5a6765734556cf6ee8fc4aae96
    Reviewed-on: https://gerrit.chromium.org/gerrit/12102
    Tested-by: Darin Petkov <petkov@chromium.org>
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-by: Richard Barnette <jrbarnette@chromium.org>
    Commit-Ready: Darin Petkov <petkov@chromium.org>

--- mod_for_test_scripts/test_setup.sh
+++ mod_for_test_scripts/test_setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Copyright (c) 2009 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
+set -e
 
 for SCRIPT in \
     ${GCLIENT_ROOT}/src/scripts/mod_for_test_scripts/[0-9][0-9][0-9]*[!$~]
