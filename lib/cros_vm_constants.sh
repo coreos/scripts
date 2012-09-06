bd0ba0c9dad0cf2be10bded4c060898737ae145d
Chris Sosa <sosa@chromium.org>
2012-09-06 14:00:20 -0700

    VDisk size must also be changed to allot for large stateful partition.
    BUG=chromium-os:34126
    TEST=Ran image_to_vm with full line from buildbot.
    
    Change-Id: I7b579500e80f3b3882f7e9aa62f73e89bf11ceba
    Reviewed-on: https://gerrit.chromium.org/gerrit/32429
    Reviewed-by: Chris Sosa <sosa@chromium.org>
    Tested-by: Chris Sosa <sosa@chromium.org>

--- lib/cros_vm_constants.sh
+++ lib/cros_vm_constants.sh
@@ -12,5 +12,5 @@ DEFAULT_VBOX_DISK="os.vdi"
 DEFAULT_QEMU_IMAGE="chromiumos_qemu_image.bin"
 
 # Minimum sizes for full size vm images -- needed for update.
-MIN_VDISK_SIZE_FULL=6072
+MIN_VDISK_SIZE_FULL=9216
 MIN_STATEFUL_FS_SIZE_FULL=6144
