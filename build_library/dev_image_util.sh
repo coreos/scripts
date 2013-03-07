7d9acd0de13c8d9ee7ec1e4a4f5c3834487d9bd0
Brandon Philips <brandon@ifup.co>
2013-03-07 11:32:13 -0800

    Revert "fix(dev_util): restore removed code from dev_util"
    so it is a bit complicated but essentially gtest pulls in python which
    pulls in pyton-updater which wants portage so portage gets installed in
    teh real root not the dev one. Just leave it for now.

--- build_library/dev_image_util.sh
+++ build_library/dev_image_util.sh
@@ -36,7 +36,6 @@ install_dev_packages() {
   sudo ln -s /usr/local/usr/lib/debug "${root_fs_dir}/usr/lib/debug"
 
   # Install the bare necessary files so that the "emerge" command works
-  sudo cp -a ${root_dev_dir}/share/portage ${root_fs_dir}/usr/share
   sudo sed -i s,/usr/bin/wget,wget, \
     ${root_fs_dir}/usr/share/portage/config/make.globals
 
