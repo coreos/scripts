37953e039b399ea6cc2c39914516511cf708df83
Brandon Philips <brandon@ifup.co>
2013-05-29 21:59:03 -0700

    fix(core_update_developer_track): fix docs and a bug
    The UUID changed for CoreOS. Make sure we use our own shiny UUID.
    
    Add a doc note that the update should be generated from the "template
    image" not from one of the vm images.

--- core_update_developer_track.sh
+++ core_update_developer_track.sh
@@ -4,8 +4,12 @@ usage="
 usage: $0 <image.bin> <api key>\n
 \n
 Setting everything up for use\n
+
 1) Run 'gsutil config' and use project id coreos.com:core-update-storage\n
 2) Ensure core-admin is installed, it is a recent addition\n
+
+NOTE: Use the chromiumos_image.bin not a qemu/xen/etc image for generating the
+update.
 "
 
 FILE=$1
@@ -24,5 +28,5 @@ fi
 
 cros_generate_update_payload --image $FILE --output /tmp/update.gz
 MD5SUM=$(md5sum $FILE | cut -f1 -d" ")
-core-admin new-version -k $KEY -v 9999.0.0 -a {87efface-864d-49a5-9bb3-4b050a7c227a} -t developer-build -p $MD5SUM /tmp/update.gz
+core-admin new-version -k $KEY -v 9999.0.0 -a {e96281a6-d1af-4bde-9a0a-97b76e56dc57} -t developer-build -p $MD5SUM /tmp/update.gz
 gsutil cp /tmp/update.gz gs://update-storage.core-os.net/developer-build/$MD5SUM/update.gz
