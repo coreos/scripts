coreos/scripts

commit feb59db9f58f1e02c0acdf6488453f826c322be1

fix(release_util): Add argument to specify the name to use for .DIGESTS
For multi-file uploads we should explicitly declare what the name of the
.DIGESTS file should be instead of using the first file name. Relying on
the ordering was subtle and easy to break.

build_library/release_util.sh
@@ -85,11 +85,24 @@ upload_packages() {
     upload_files packages ${def_upload_path} "pkgs/" "${board_packages}"/*
 }
 
-# Upload a image along with optional supporting files
-# The image file must be the first argument
+# Upload a set of files (usually images) and digest, optionally w/ gpg sig
+# If more than one file is specified -d must be the first argument
+# Usage: upload_image [-d file.DIGESTS] file1 [file2...]
 upload_image() {
     [[ ${FLAGS_upload} -eq ${FLAGS_TRUE} ]] || return 0
     [[ -n "${BOARD}" ]] || die "board_options.sh must be sourced first"
+
+    # The name to use for .DIGESTS and .DIGESTS.asc must be explicit if
+    # there is more than one file to upload to avoid potential confusion.
+    local digests
+    if [[ "$1" == "-d" ]]; then
+        [[ -n "$2" ]] || die "-d requires an argument"
+        digests="$2"
+        shift 2
+    else
+        [[ $# -eq 1 ]] || die "-d is required for multi-file uploads"
+        digests="${1}.DIGESTS"
+    fi
 
     local uploads=()
     local filename
@@ -110,18 +123,18 @@ upload_image() {
 
     # For consistency generate a .DIGESTS file similar to the one catalyst
     # produces for the SDK tarballs and up upload it too.
-    make_digests "${uploads[@]}"
-    uploads+=( "${uploads[0]}.DIGESTS" )
+    make_digests -d "${digests}" "${uploads[@]}"
+    uploads+=( "${digests}" )
 
     # Create signature as ...DIGESTS.asc as Gentoo does.
     if [[ -n "${FLAGS_sign_digests}" ]]; then
-      rm -f "${uploads[0]}.DIGESTS.asc"
+      rm -f "${digests}.asc"
       gpg --batch --local-user "${FLAGS_sign_digests}" \
-          --clearsign "${uploads[0]}.DIGESTS" || die "gpg failed"
-      uploads+=( "${uploads[0]}.DIGESTS.asc" )
+          --clearsign "${digests}" || die "gpg failed"
+      uploads+=( "${digests}.asc" )
     fi
 
-    local log_msg="${1##*/}"
+    local log_msg=$(basename "$digests" .DIGESTS)
     local def_upload_path="${UPLOAD_ROOT}/${BOARD}/${COREOS_VERSION_STRING}"
     upload_files "${log_msg}" "${def_upload_path}" "" "${uploads[@]}"
 }

build_library/vm_image_util.sh
@@ -632,7 +632,8 @@ vm_cleanup() {
 }
 
 vm_upload() {
-    upload_image "${VM_GENERATED_FILES[@]}"
+    local digests="${VM_GENERATED_FILES[0]}.DIGESTS"
+    upload_image -d "${digests}" "${VM_GENERATED_FILES[@]}"
 }
 
 print_readme() {

build_image
@@ -170,7 +170,8 @@ COREOS_PATCH=${COREOS_PATCH}
 COREOS_SDK_VERSION=${COREOS_SDK_VERSION}
 EOF
 
-upload_image "${BUILD_DIR}/au-generator.zip" "${BUILD_DIR}/version.txt"
+upload_image -d "${BUILD_DIR}/au-generator.zip.DIGESTS" \
+    "${BUILD_DIR}/au-generator.zip" "${BUILD_DIR}/version.txt"
 
 # Create a named symlink.
 LINK_NAME="${FLAGS_output_root}/${BOARD}/${FLAGS_symlink}"

common.sh
@@ -693,40 +693,45 @@ enable_rw_mount() {
 
 # Generate a DIGESTS file, as normally used by Gentoo.
 # This is an alternative to shash which doesn't know how to report errors.
-# Usage: make_digests file1 [file2...]
-# Output: file1.DIGESTS
-# Any extra files be hashed and listed in file1.DIGESTS
+# Usage: make_digests -d file.DIGESTS file1 [file2...]
 _digest_types="md5 sha1 sha512"
 make_digests() {
-    local dirname=$(dirname "$1")
-    local basename=$(basename "$1")
+    [[ "$1" == "-d" ]] || die
+    local digests="$(readlink -f "$2")"
+    shift 2
 
-    pushd "${dirname}" >/dev/null
-    echo -n > "${basename}.DIGESTS"
+    pushd "$(dirname "$1")" >/dev/null
+    echo -n > "${digests}"
     for filename in "$@"; do
         filename=$(basename "$filename")
         info "Computing DIGESTS for ${filename}"
         for hash_type in $_digest_types; do
-            echo "# $hash_type HASH" | tr "a-z" "A-Z" >> "${basename}.DIGESTS"
-            ${hash_type}sum "${filename}" >> "${basename}.DIGESTS"
+            echo "# $hash_type HASH" | tr "a-z" "A-Z" >> "${digests}"
+            ${hash_type}sum "${filename}" >> "${digests}"
         done
     done
     popd >/dev/null
 }
 
 # Validate a DIGESTS file. Essentially the inverse of make_digests.
-# Usage: verify_digests file1 [file2...]
-# Checks the hash of all given files using file1.DIGESTS
+# Usage: verify_digests [-d file.DIGESTS] file1 [file2...]
+# If -d is not specified file1.DIGESTS will be used
 verify_digests() {
-    local dirname=$(dirname "$1")
-    local basename=$(basename "$1")
+    local digests
+    if [[ "$1" == "-d" ]]; then
+        [[ -n "$2" ]] || die "-d requires an argument"
+        digests="$(readlink -f "$2")"
+        shift 2
+    else
+        digests=$(basename "${1}.DIGESTS")
+    fi
 
-    pushd "${dirname}" >/dev/null
+    pushd "$(dirname "$1")" >/dev/null
     for filename in "$@"; do
         filename=$(basename "$filename")
         info "Validating DIGESTS for ${filename}"
         for hash_type in $_digest_types; do
-            grep -A1 -i "^# ${hash_type} HASH$" "${basename}.DIGESTS" | \
+            grep -A1 -i "^# ${hash_type} HASH$" "${digests}" | \
                 grep "$filename$" | ${hash_type}sum -c - --strict || return 1
             # Also check that none of the greps failed in the above pipeline
             [[ -z ${PIPESTATUS[*]#0} ]] || return 1

commit 1177d198da85fe35ed42466100afcee11d0f899c

fix(release_util): Fix DIGESTS regression from feb59db9f
While attempting to fix the easy to mix up DIGESTS names in feb59db9f I
stumbled across yet another way that the DIGESTS names were a bit
unpredictable: previously a .bz2 got stuck into the file name when
upload_images automatically compressed some file types. The new code
missed this and never added the .bz2. Correct this now, both for
image_to_vm which has a pile of glue to keep the legacy names in place
for now and build_image which I never intended to change.

build_library/release_util.sh
@@ -114,7 +114,7 @@ upload_image() {
         shift 2
     else
         [[ $# -eq 1 ]] || die "-d is required for multi-file uploads"
-        digests="${1}.DIGESTS"
+        # digests is assigned after image is possibly compressed/renamed
     fi
 
     local uploads=()
@@ -133,6 +133,10 @@ upload_image() {
             uploads+=( "${filename}" )
         fi
     done
+
+    if [[ -z "${digests}" ]]; then
+        digests="${uploads[0]}.DIGESTS"
+    fi
 
     # For consistency generate a .DIGESTS file similar to the one catalyst
     # produces for the SDK tarballs and up upload it too.

build_library/vm_image_util.sh
@@ -645,20 +645,26 @@ vm_upload() {
     # consistent and relying on ordering was breakable.
     # Now the common prefix, output by $(_dst_name) is used above.
     # Some download/install scripts may still refer to the old name.
-    local uploaded legacy_digests
+    local uploaded legacy_uploaded
     for uploaded in "${VM_GENERATED_FILES[@]}"; do
         if [[ "${uploaded}" == "${VM_DST_IMG}" ]]; then
-            legacy_digests="$(_dst_dir)/$(basename ${VM_DST_IMG}).DIGESTS"
+            legacy_uploaded="$(_dst_dir)/$(basename ${VM_DST_IMG})"
             break
         fi
     done
 
     # Since depending on the ordering of $VM_GENERATED_FILES is brittle only
     # use it if $VM_DST_IMG isn't included in the uploaded files.
-    if [[ -z "${legacy_digests}" ]]; then
-        legacy_digests="${VM_GENERATED_FILES[0]}.DIGESTS"
+    if [[ -z "${legacy_uploaded}" ]]; then
+        legacy_uploaded="${VM_GENERATED_FILES[0]}"
     fi
 
+    # If upload_images compressed $legacy_uploaded be sure to add .bz2
+    if [[ "${legacy_uploaded}" =~ \.(img|bin|vdi|vmdk)$ ]]; then
+        legacy_uploaded+="${IMAGE_ZIPEXT}"
+    fi
+
+    local legacy_digests="${legacy_uploaded}.DIGESTS"
     [[ "${legacy_digests}" != "${digests}" ]] || return 0
 
     local legacy_uploads=( "${legacy_digests}" )