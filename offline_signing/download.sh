f811845778c64b6197e8e3a72a9dcefe0ebf2f02
Michael Marineau <michael.marineau@coreos.com>
2014-06-24 15:22:38 -0700

    updates: add example scripts for signing update payloads
    This pretty much just translates the signing instructions into some
    trivial wrapper scripts to serve as an example on how to do this.

---
+++ offline_signing/download.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+VERSION="$1"
+GS="gs://builds.release.core-os.net/alpha/boards/amd64-usr/$VERSION"
+
+cd "${2:-.}"
+
+gsutil cp \
+    "${GS}/coreos_production_update.bin.bz2" \
+    "${GS}/coreos_production_update.bin.bz2.sig" \
+    "${GS}/coreos_production_update.zip" \
+    "${GS}/coreos_production_update.zip.sig" ./
+
+gpg --verify "coreos_production_update.bin.bz2.sig"
+gpg --verify "coreos_production_update.zip.sig"
