f1c81df7f386dc8af9d601b1f147d10a33468aa5
Michael Marineau <michael.marineau@coreos.com>
2014-06-25 22:20:56 -0700

    updates: minor fixes for offline signing wrapper script
    - Fedora provides gpg2 by default, not gpg.
    - The zip file's root must be the current directory.

--- offline_signing/sign.sh
+++ offline_signing/sign.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 set -ex
-DATA_DIR="$1"
+DATA_DIR="$(readlink -f "$1")"
+KEYS_DIR="$(readlink -f "$(dirname "$0")")"
 
-gpg --verify "${DATA_DIR}/coreos_production_update.bin.bz2.sig"
-gpg --verify "${DATA_DIR}/coreos_production_update.zip.sig"
+gpg2 --verify "${DATA_DIR}/coreos_production_update.bin.bz2.sig"
+gpg2 --verify "${DATA_DIR}/coreos_production_update.zip.sig"
 bunzip2 --keep "${DATA_DIR}/coreos_production_update.bin.bz2"
 unzip "${DATA_DIR}/coreos_production_update.zip" -d "${DATA_DIR}"
 
 export PATH="${DATA_DIR}:${PATH}"
 
-core_sign_update \
+cd "${DATA_DIR}"
+./core_sign_update \
     --image "${DATA_DIR}/coreos_production_update.bin" \
-    --output "${DATA_DIR}/update.gz" \
-    --private_keys "devel.key.pem:prod-2.key.pem" \
-    --public_keys  "devel.pub.pem:prod-2.pub.pem"
+    --output "${DATA_DIR}/coreos_production_update.gz" \
+    --private_keys "${KEYS_DIR}/devel.key.pem:${KEYS_DIR}/prod-2.key.pem" \
+    --public_keys  "${KEYS_DIR}/devel.pub.pem:${KEYS_DIR}/prod-2.pub.pem"
