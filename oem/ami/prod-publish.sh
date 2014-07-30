f72d744a7a5161448e7c3bbaf06e8ba8c1378ed7
Michael Marineau <michael.marineau@coreos.com>
2014-07-30 14:06:44 -0700

    amis: switch back to sda for root PV volume, add publish wrapper
    If additional EBS volumes are mapped to a PV instance using a "sd*" name
    they will always be ordered by the hypervisor before "xvd*" devices,
    again ignoring the root device definition. This applies to all PV
    instance types so we cannot get away with just poo-pooing m1.small.
    
    We will need to call attention to this since it requires users who set
    the volume size via APIs to use the name "/dev/sda" again.

---
+++ oem/ami/prod-publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BOARD="amd64-usr"
+GROUP="$1"
+VER="$2"
+DIR=/home/ec2-user/scripts
+
+if [ -z "$GROUP" -o -z "$VER" ]; then
+  echo "Usage: $0 alpha 1.2.3" >&2
+  exit 1
+fi
+
+set -e
+source $DIR/marineam-auth.sh
+$DIR/publish_ami.sh -b $BOARD -g $GROUP -V $VER"
