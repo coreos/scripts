coreos/scripts

commit 8abceaa20e10ff5f3543a54e69b5accddfd5f7b9

scripts/oem/ami: consolidate regions and AKI ids

oem/ami/copy_ami.sh
@@ -12,17 +12,8 @@
 # Set pipefail along with -e in hopes that we catch more errors
 set -e -o pipefail
 
-declare -A AKI
-AKI["us-east-1"]=aki-b4aa75dd 
-AKI["us-west-1"]=aki-eb7e26ae
-AKI["us-west-2"]=aki-f837bac8 
-AKI["eu-west-1"]=aki-8b655dff
-AKI["eu-central-1"]=aki-184c7a05
-AKI["ap-southeast-1"]=aki-fa1354a8
-AKI["ap-southeast-2"]=aki-3d990e07
-AKI["ap-northeast-1"]=aki-40992841
-AKI["sa-east-1"]=aki-c88f51d5
-# AKI["gov-west-1"]=aki-75a4c056
+DIR=$(dirname $0)
+source $DIR/regions.sh
 
 USAGE="Usage: $0 -a ami-id
     -a ami-id   ID of the AMI to be coppied.
@@ -42,7 +33,7 @@ GROUP="alpha"
 REGIONS=()
 
 add_region() {
-    if [[ -z "${AKI[$1]}" ]]; then
+    if [[ -z "${ALL_REGIONS[$1]}" ]]; then
         echo "Invalid region '$1'" >&2;
         exit 1
     fi
@@ -105,7 +96,7 @@ else
 fi
 
 if [[ ${#REGIONS[@]} -eq 0 ]]; then
-    REGIONS=( "${!AKI[@]}" )
+    REGIONS=( "${ALL_REGIONS[@]}" )
 fi
 
 # The name has a limited set of allowed characterrs

oem/ami/build_ebs_on_ec2.sh
@@ -5,24 +5,11 @@
 # mad props to Eric Hammond for the initial script
 #  https://github.com/alestic/alestic-hardy-ebs/blob/master/bin/alestic-hardy-ebs-build-ami
 
-# AKI ids from:
-#  http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/UserProvidedkernels.html
-# we need pv-grub-hd00 x86_64
-
 # Set pipefail along with -e in hopes that we catch more errors
 set -e -o pipefail
 
-declare -A AKI
-AKI["us-east-1"]=aki-b4aa75dd 
-AKI["us-west-1"]=aki-eb7e26ae
-AKI["us-west-2"]=aki-f837bac8 
-AKI["eu-west-1"]=aki-8b655dff
-AKI["eu-central-1"]=aki-184c7a05
-AKI["ap-southeast-1"]=aki-fa1354a8
-AKI["ap-southeast-2"]=aki-3d990e07
-AKI["ap-northeast-1"]=aki-40992841
-AKI["sa-east-1"]=aki-c88f51d5
-# AKI["gov-west-1"]=aki-75a4c056
+DIR=$(dirname $0)
+source $DIR/regions.sh
 
 readonly COREOS_EPOCH=1372636800
 VERSION="master"
@@ -110,7 +97,7 @@ description="CoreOS $GROUP $VERSION"
 zoneurl=http://instance-data/latest/meta-data/placement/availability-zone
 zone=$(curl --fail -s $zoneurl)
 region=$(echo $zone | sed 's/.$//')
-akiid=${AKI[$region]}
+akiid=${ALL_AKIS[$region]}
 
 if [ -z "$akiid" ]; then
    echo "$0: Can't identify AKI, using region: $region" >&2

oem/ami/publish_ami.sh
@@ -3,17 +3,8 @@
 # Set pipefail along with -e in hopes that we catch more errors
 set -e -o pipefail
 
-REGIONS=(
-    us-east-1
-    us-west-1
-    us-west-2
-    eu-west-1
-    eu-central-1
-    ap-southeast-1
-    ap-southeast-2
-    ap-northeast-1
-    sa-east-1
-)
+DIR=$(dirname $0)
+source $DIR/regions.sh
 
 USAGE="Usage: $0 -V 100.0.0
     -V VERSION  Find AMI by CoreOS version. (required)
@@ -63,7 +54,7 @@ fi
 
 search_name=$(clean_version "CoreOS-$GROUP-$VER")
 declare -A AMIS HVM_AMIS
-for r in "${REGIONS[@]}"; do
+for r in "${ALL_REGIONS[@]}"; do
     AMI=$(ec2-describe-images --region=${r} -F name="${search_name}" \
         | grep -m1 ^IMAGE | cut -f2) || true
     if [[ -z "$AMI" ]]; then

oem/ami/regions.sh
@@ -1,0 +1,17 @@
+# AKI ids from:
+#  http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/UserProvidedkernels.html
+# we need pv-grub-hd00 x86_64
+
+declare -A ALL_AKIS
+ALL_AKIS["us-east-1"]=aki-b4aa75dd
+ALL_AKIS["us-west-1"]=aki-eb7e26ae
+ALL_AKIS["us-west-2"]=aki-f837bac8
+ALL_AKIS["eu-west-1"]=aki-8b655dff
+ALL_AKIS["eu-central-1"]=aki-184c7a05
+ALL_AKIS["ap-southeast-1"]=aki-fa1354a8
+ALL_AKIS["ap-southeast-2"]=aki-3d990e07
+ALL_AKIS["ap-northeast-1"]=aki-40992841
+ALL_AKIS["sa-east-1"]=aki-c88f51d5
+# ALL_AKIS["gov-west-1"]=aki-75a4c056
+
+ALL_REGIONS=( "${!ALL_AKIS[@]}" )

commit febf7a064ebf23a9aa27de445f66d9c05df59a37

scripts/oem/ami: fix bad array subscript
Broken in 8abceaa20e10ff5f3543a54e69b5accddfd5f7b9.

oem/ami/copy_ami.sh
@@ -33,7 +33,7 @@ GROUP="alpha"
 REGIONS=()
 
 add_region() {
-    if [[ -z "${ALL_REGIONS[$1]}" ]]; then
+    if [[ -z "${ALL_AKIS[$1]}" ]]; then
         echo "Invalid region '$1'" >&2;
         exit 1
     fi