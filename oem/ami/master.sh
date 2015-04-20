1508b44722505ce9317b09f2aec0e02de0a34421
Michael Marineau <michael.marineau@coreos.com>
2015-04-20 13:43:59 -0700

    oem/ami: add new import script, replaces build_ebs_on_ec2.sh
    This script uses the EC2 volume import tools instead of attaching and
    writing to an EBS volume. This mechanism will be useful for creating
    AMIs in isolated EC2 regions and can be run from any host with API
    access and the EC2 tools.
    
    TODO: Allow region to be specified and automatically create region-local
    S3 buckets as needed. This version hard codes a bucket only usable by
    our dev AWS account, not prod. Later on: move to a more compact disk
    format like VMDK.

--- oem/ami/master.sh
+++ oem/ami/master.sh
@@ -8,6 +8,6 @@ eval $(curl -f "${URL}/version.txt")
 
 source $DIR/marineam-auth.sh
 args="-b amd64-usr -g master -V ${COREOS_VERSION}"
-$DIR/build_ebs_on_ec2.sh ${args} -u "${URL}/coreos_production_ami_image.bin.bz2"
+$DIR/import.sh ${args} -u "${URL}/coreos_production_ami_image.bin.bz2"
 $DIR/test_ami.sh -v ${args}
 #$DIR/copy_ami.sh ${args}
