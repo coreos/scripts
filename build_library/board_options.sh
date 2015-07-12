5985b1e3d6c5bc0c34c8cb3a6909ef06cd9a606b
Michael Marineau <michael.marineau@coreos.com>
2015-07-12 15:50:58 -0700

    build_library: replace package checking script
    The new python script check_root uses data that portage already
    maintains on what shared libraries packages need or provide instead of
    re-scanning whatever ELF files that can be found. This is much more
    comprehensive but there is a bit of a transition issue for folks with
    long-lived SDKs: packages built with portage older than 2.2.18 do not
    include this data. As such for now the check is non-fatal and provides a
    command you can use to refresh locally installed packages.
    
    The code checking for conflicts between top level directories and /usr
    has also been rewritten. Both tests now are considerably faster.

--- build_library/board_options.sh
+++ build_library/board_options.sh
@@ -24,3 +24,31 @@ pkg_use_enabled() {
   equery-"${BOARD}" -q uses "${pkg}" | grep -q ${grep_args}
   return $?
 }
+
+# get a package's SONAMEs in soname.provided format
+pkg_soname_provides() {
+  local provides p
+  # We could run this command but it ugly and silly slow:
+  # portageq-"${BOARD}" metadata "${BOARD_ROOT}" installed "$1" PROVIDES
+  provides=$(<"${BOARD_ROOT}/var/db/pkg/$1/PROVIDES")
+
+  if [[ -z "$provides" ]]; then
+    return
+  fi
+
+  # convert:
+  #  x86_32: libcom_err.so.2 libss.so.2 x86_64: libcom_err.so.2 libss.so.2
+  # into:
+  #  x86_32 libcom_err.so.2 libss.so.2
+  #  x86_64 libcom_err.so.2 libss.so.2
+  echo -n "# $1:"
+  for p in ${provides}; do
+    if [[ "$p" == *: ]]; then
+      echo
+      echo -n "${p%:}"
+    else
+      echo -n " $p"
+    fi
+  done
+  echo
+}
