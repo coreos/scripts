d6b951c4160800a1deabc3639d4497ecea9146a0
Michael Marineau <michael.marineau@coreos.com>
2016-09-26 13:44:45 -0700

    offline_signing: vmlinuz is not bz2 compressed

--- offline_signing/download.sh
+++ offline_signing/download.sh
@@ -7,13 +7,13 @@ GS="gs://builds.release.core-os.net/stable/boards/amd64-usr/$VERSION"
 cd "${2:-.}"
 
 gsutil cp \
-    "${GS}/coreos_production_image.vmlinuz.bz2" \
-    "${GS}/coreos_production_image.vmlinuz.bz2.sig" \
+    "${GS}/coreos_production_image.vmlinuz" \
+    "${GS}/coreos_production_image.vmlinuz.sig" \
     "${GS}/coreos_production_update.bin.bz2" \
     "${GS}/coreos_production_update.bin.bz2.sig" \
     "${GS}/coreos_production_update.zip" \
     "${GS}/coreos_production_update.zip.sig" ./
 
-gpg --verify "coreos_production_image.vmlinuz.bz2.sig"
+gpg --verify "coreos_production_image.vmlinuz.sig"
 gpg --verify "coreos_production_update.bin.bz2.sig"
 gpg --verify "coreos_production_update.zip.sig"
