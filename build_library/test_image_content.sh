bd79eaf9d189b991a15e7f22502971c819ba0a4d
Benjamin Gilbert <bgilbert@redhat.com>
2019-09-10 21:47:55 -0400

    test_image_content: whitelist Perl and dbus GLSAs
    Backported the dbus GLSA.  Ignoring the Perl one.

--- build_library/test_image_content.sh
+++ build_library/test_image_content.sh
@@ -5,6 +5,8 @@
 GLSA_WHITELIST=(
 	201412-09 # incompatible CA certificate version numbers
 	201908-14 # backported both CVE fixes
+	201909-01 # Perl, SDK only
+	201909-08 # backported fix
 )
 
 glsa_image() {
